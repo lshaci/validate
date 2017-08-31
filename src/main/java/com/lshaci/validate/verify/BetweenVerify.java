3204344659266d74bad29e52689b6d7d01e3701c
lshaci <lshaci@qq.com>
2017-08-31 11:54:25 +0800

    optimization between

--- src/main/java/com/lshaci/validate/verify/BetweenVerify.java
+++ src/main/java/com/lshaci/validate/verify/BetweenVerify.java
@@ -1,6 +1,8 @@
 package com.lshaci.validate.verify;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,8 @@ public class BetweenVerify implements Verify {
 	private static final Logger logger = LoggerFactory.getLogger(BetweenVerify.class);
 	
 	private static final String verify = Between.class.getSimpleName();
+	
+	private static Map<String, Integer> cache = new HashMap<>();
 
 	@Override
 	public ValidateMessage validate(Field field, Object value) {
@@ -33,10 +37,8 @@ public class BetweenVerify implements Verify {
 			}
 		}
 		if (value instanceof Integer) {
-			Integer min = ParamUtils.getInt(between.min());
-			Integer max = ParamUtils.getInt(between.max());
-			min = min != null ? min : Integer.parseInt(Between.MIN_DEFAULT);
-			max = max != null ? max : Integer.parseInt(Between.MAX_DEFAULT);
+			int min = getMin(between);
+			int max = getMax(between);
 			if ((int) value < min || (int) value  > max) {
 				logger.error("The value of this field({}) exceeds the limit [{}-{}]!", field.getName(), min, max);
 				return new ValidateMessage(verify, field.getName(), between.message() + "[" + min + "-" + max + "]");
@@ -48,6 +50,42 @@ public class BetweenVerify implements Verify {
 		
 		logger.debug(SUCCESS, field.getName());
 		return null;
+	}
+	
+	/**
+	 * 获取Between注解的最小值
+	 * 
+	 * @param between	between.min()
+	 * @return
+	 */
+	private static int getMin(Between between) {
+		return getValue(between.min(), Between.MIN_DEFAULT);
+	}
+	/**
+	 * 获取Between注解的最大值
+	 * 
+	 * @param between	between.max()
+	 * @return
+	 */
+	private static int getMax(Between between) {
+		return getValue(between.max(), Between.MAX_DEFAULT);
+	}
+	
+	/**
+	 * 获取Between注解的值
+	 * 
+	 * @param key
+	 * @param defaultValue
+	 * @return
+	 */
+	private static int getValue(String key, String defaultValue) {
+		Integer value = cache.get(key);
+		if (value == null) {
+			value = ParamUtils.getInt(key);
+			value = value != null ? value : Integer.parseInt(defaultValue);
+			cache.put(key, value);
+		}
+		return value.intValue();
 	}
 
 }
