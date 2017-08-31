e63a89a701e8c93beda109db6a8b2450b4899e5b
lshaci <lshaci@Dan>
2017-08-31 22:45:13 +0800

    修改bug

--- src/main/java/com/lshaci/validate/ValidatorFactory.java
+++ src/main/java/com/lshaci/validate/ValidatorFactory.java
@@ -3,8 +3,14 @@ package com.lshaci.validate;
 import static java.util.stream.Collectors.toList;
 
 import java.io.File;
-import java.util.Arrays;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Validator Factory
@@ -12,6 +18,8 @@ import java.util.List;
  * @author lshaci
  */
 public class ValidatorFactory {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ValidatorFactory.class);
 	
 	/**
 	 * validate annotation path
@@ -22,18 +30,30 @@ public class ValidatorFactory {
 	 * Build a Validator, The validate annotation in ./annotation 
 	 * 
 	 * @return The default Validator
+	 * @throws IOException 
 	 */
 	public static Validator buildDefaultValidator() {
 		String packageName = ValidatorFactory.class.getPackage().getName() + "." + ANNOTATION_PATH + ".";
-		String currentPath = ValidatorFactory.class.getResource(".").getPath();
-		File file = new File(currentPath, ANNOTATION_PATH);
-		
-		List<String> annotationClassNames = Arrays.stream(file.listFiles())
-			.map(File::getName)
-			.filter(e -> e.endsWith(".class"))
-			.map(e -> packageName + e.substring(0, e.indexOf(".")))
-			.collect(toList());
-		
+		String path = packageName.replace(".", "/");
+		List<File> files = new ArrayList<>();
+		Enumeration<URL> resources;
+		try {
+			resources = Thread.currentThread().getContextClassLoader().getResources(path);
+		} catch (IOException e) {
+			logger.warn("Ignore this exception!");
+			throw new RuntimeException();
+		}
+		while (resources.hasMoreElements()) {
+			URL url = (URL) resources.nextElement();
+			files.add(new File(url.getPath()));
+		}
+		List<String> annotationClassNames = files.stream()
+				.filter(File::isDirectory)
+				.map(File::getName)
+				.filter(e -> e.endsWith(".class"))
+				.map(e -> packageName + e.substring(0, e.indexOf(".")))
+				.collect(toList());
 		return new Validator(annotationClassNames);
+		
 	}
 }
